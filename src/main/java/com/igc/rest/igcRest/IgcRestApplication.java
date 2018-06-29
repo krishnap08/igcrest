5543fd9fed3c8bc64a0e90f8396c8266ffc467f3
krishnap08 <krishnap08@gmail.com>
2018-06-29 08:48:51 +0530

    initial commit

---
+++ src/main/java/com/igc/rest/igcRest/IgcRestApplication.java
@@ -0,0 +1,12 @@
+package com.igc.rest.igcRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgcRestApplication.class, args);
+	}
+}
